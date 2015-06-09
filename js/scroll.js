9f0aca4895faf88a47b0a9d3cf9fa308c1f00862
tywzdbsd <370464169@qq.com>
2015-06-09 21:25:53 +0800

    add Scrollify

--- js/scroll.js
+++ js/scroll.js
@@ -9,5 +9,6 @@ $(function() {
             anchors: ['page1', 'page2', 'page3', 'page4','page5','page6','page7'],
             menu: '#menu'
             //'navigation': true  //have scroll bar
-        });
+        }
+    );
 });
